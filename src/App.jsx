36e30694fcda36ec056bf27c4277c1f66873a35a
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-01-11 11:25:33 +0530

    Initial commit for React project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  
+
+  return (
+    <>
+      <ListEmployeeComponent/>
+    </>
+  )
+}
+
+export default App
